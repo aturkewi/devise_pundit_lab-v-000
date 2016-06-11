5f9e4403d94ade5f815021ce4f3190bdf5309d31
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-11 13:06:35 -0400

    Add users index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users' => 'users#index', as: 'users'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
